e5524499e104d3329d3d8d4c1e293463589139a0
Patrick Morgan <patrick.morgan@live.com.au>
2017-09-16 18:16:51 +1000

    Pulling events from response

--- app/containers/App.js
+++ app/containers/App.js
@@ -1,13 +1,52 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import axios from 'axios';
+import _ from 'lodash';
 
 class App extends Component {
-  render () {
-    return (
-      <div>
-        Hello World!
-      </div>
-    )
-  }
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: null
+        };
+    }
+
+    componentDidMount() {
+        this.getData();
+    }
+
+    getData() {
+        axios.get('../../resources/test.json')
+            .then((response) => {
+                const data = JSON.parse(response.data.processingResult);
+                this.setState({data});
+
+                console.log(data);
+                let events = [];
+                _.map(data.fragments, (fragment) => {
+                    _.map(fragment.events, (event) => {
+                        events.push(event[0].scores);
+                    });
+                });
+                console.log(events);
+            });
+    }
+
+    render() {
+        if (!this.state.data) {
+            return (
+                <div>
+                    Loading...
+                </div>
+            );
+        }
+
+        return (
+            <div>
+                <Chart data={} />
+            </div>
+        );
+    }
 }
 
 export default App;
