28da305c3ad720c5c15ae31967ad6753f4c7f036
Patrick Morgan <patrick.morgan@live.com.au>
2017-09-19 19:47:32 +1000

    Proof of concept complete

--- app/containers/App.js
+++ app/containers/App.js
@@ -2,38 +2,62 @@ import React, {Component} from 'react';
 import axios from 'axios';
 import _ from 'lodash';
 
+import Chart from '../components/Chart';
+
 class App extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            data: null
+            data: null,
+            events: null
         };
     }
 
-    componentDidMount() {
+    componentWillMount() {
         this.getData();
     }
 
     getData() {
+        // axios.get('test.json')
         axios.get('../../resources/test.json')
             .then((response) => {
                 const data = JSON.parse(response.data.processingResult);
                 this.setState({data});
 
-                console.log(data);
-                let events = [];
-                _.map(data.fragments, (fragment) => {
-                    _.map(fragment.events, (event) => {
-                        events.push(event[0].scores);
+                let events = {
+                    anger: [],
+                    contempt: [],
+                    disgust: [],
+                    fear: [],
+                    happiness: [],
+                    neutral: [],
+                    sadness: [],
+                    surprise: [],
+                };
+                let time = 0;
+                const timescale = data.timescale;
+                _.map(data.fragments, fragment => {
+                    const interval = fragment.interval;
+                    _.map(fragment.events, event => {
+                        time = _.round(time, 2);
+                        events.anger.push({x: time, y: _.round(event[0].scores.anger * 100)});
+                        events.contempt.push({x: time, y: _.round(event[0].scores.contempt * 100)});
+                        events.disgust.push({x: time, y: _.round(event[0].scores.disgust * 100)});
+                        events.fear.push({x: time, y: _.round(event[0].scores.fear * 100)});
+                        events.happiness.push({x: time, y: _.round(event[0].scores.happiness * 100)});
+                        events.neutral.push({x: time, y: _.round(event[0].scores.neutral * 100)});
+                        events.sadness.push({x: time, y: _.round(event[0].scores.sadness * 100)});
+                        events.surprise.push({x: time, y: _.round(event[0].scores.surprise * 100)});
+                        time += (interval / timescale);
                     });
                 });
-                console.log(events);
+                this.setState({events});
             });
     }
 
     render() {
-        if (!this.state.data) {
+        if (!this.state.events) {
             return (
                 <div>
                     Loading...
@@ -43,7 +67,15 @@ class App extends Component {
 
         return (
             <div>
-                <Chart data={} />
+                Emotional User Testing
+                <div id="result">
+                    <div id="chart-wrapper">
+                        <Chart data={this.state.events} />
+                    </div>
+                    <div id="video-wrapper">
+                        <iframe id="video" width="300" height="600" src="https://www.youtube.com/embed/23AHuCfHpbM?mute=1"></iframe>
+                    </div>
+                </div>
             </div>
         );
     }
